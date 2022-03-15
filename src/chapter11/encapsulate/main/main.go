cd3d9085528d3c61edc0966dbe3315d89206bbc0
wxcdz <812150635@qq.com>
2022-03-15 22:24:43 +0800

    封装的使用

---
+++ src/chapter11/encapsulate/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"GoCourse/src/chapter11/encapsulate/model"
+	"fmt"
+)
+
+func main() {
+
+	p := model.NewPerson("smith")
+	p.SetAge(18)
+	p.SetSal(5000)
+	fmt.Println(p)
+	fmt.Println(p.Name, " age =", p.GetAge(), " sal = ", p.GetSal())
+
+}
